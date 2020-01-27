57be1c12fe4f5cbffe5a7ff4a3b2759f25b46b2a
Jonay Gilabert López <jgl58@alu.ua.es>
2020-01-27 18:02:38 +0100

    Initial Commit

---
+++ Multimedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multimedia
+//
+//  Created by Jonay Gilabert López on 27/01/2020.
+//  Copyright © 2020 Jonay Gilabert López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
